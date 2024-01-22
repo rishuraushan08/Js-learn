ad8e95d4f2af151b7edc250d16139885298a80b0
rishuraushan08 <141109205+rishuraushan08@users.noreply.github.com>
2024-01-22 10:09:01 +0000

    datatype and variabled

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,2 +1,9 @@
 console.log("hii")
 console.log("hello")
+let dta="steysdhc";
+console.log(typeof dta)
+console.log(typeof undefined)//undefined
+console.log(typeof null)//object
+console.log(typeof object);//undefined
+console.log(typeof strng)
+console.log([typeof dta,typeof null,typeof object,typeof strng])//[ 'string', 'object', 'undefined', 'undefined' ]
