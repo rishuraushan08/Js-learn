3d37c43e6787039278c601fb565ce6ccd93d5eb5
rishuraushan08 <141109205+rishuraushan08@users.noreply.github.com>
2024-01-23 10:07:55 +0000

    datatype and string

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,9 +1,34 @@
-console.log("hii")
-console.log("hello")
-let dta="steysdhc";
-console.log(typeof dta)
-console.log(typeof undefined)//undefined
-console.log(typeof null)//object
-console.log(typeof object);//undefined
-console.log(typeof strng)
-console.log([typeof dta,typeof null,typeof object,typeof strng])//[ 'string', 'object', 'undefined', 'undefined' ]
+// console.log("hii")
+// console.log("hello")
+// let dta="steysdhc";
+// console.log(typeof dta)
+// console.log(typeof undefined)//undefined
+// console.log(typeof null)//object
+// console.log(typeof object);//undefined
+//console.log(typeof String)
+
+// console.log([typeof dta,typeof null,typeof object,typeof string])//[ 'string', 'object', 'undefined', 'undefined' ]
+// console.log(null==0);//false
+// console.log(null>=0);//true
+// console.log(null>0);//false
+// console.log(undefined>0);
+// console.log(undefined>=0);
+// console.log(undefined==0);
+//console.log("true">false);
+// const id=Symbol('12')
+// const anotherId=Symbol('12')
+// console.log(id===anotherId)//false
+// const names="raushan";
+// const newname =names.toUpperCase()
+// const age=23;
+// console.log(`Hello my name is ${newname} and my age is ${age}`);
+//console.log(names[0])
+//console.log(Math.random());//0.232521970888555
+//console.log(Math.random()*10);//5.22619779043795
+//console.log((Math.random()*10)+1);//to avoid zero 
+//console.log(Math.floor((Math.random()*10)+1));//3 to get the whole number
+
+/*//to get random value within certain range
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1)+min))*/
