4e972bcf0d059b7246e2f9c96dab7ceb4b9d5203
rishuraushan08 <141109205+rishuraushan08@users.noreply.github.com>
2024-01-31 06:13:04 +0000

    more on object and function

--- 02_basics/object.js
+++ 02_basics/object.js
@@ -26,3 +26,34 @@ myData.email="rishuraushan2000@gmail.com"//email get changed
 // console.log(myData.greeting())
 //const newUSer=new Object();//singleton Object
 //const newUSer={};//non Singelton Object
+
+const obj1={1:"a",2:"b"}
+const obj2={3:"c",4:"d"}
+// const obj3={obj1, obj2}
+// console.log(obj3);//{ obj1: { '1': 'a', '2': 'b' }, obj2: { '3': 'c', '4': 'd' } }
+const obj3=Object.assign({},obj1, obj2);//{}->to make sure we are creating a new object as this empty paren.. will be the target object in which all object will get merged
+//console.log(obj3);//{ '1': 'a', '2': 'b', '3': 'c', '4': 'd' }
+
+const user ={
+     id:1,
+     name:"raushan",
+     email:"rauhsanlive2000@gmail.com"
+ }
+
+// console.log(Object.keys(user))//[ 'id', 'name', 'email' ]
+// console.log(Object.values(user))//[ 1, 'raushan', 'rauhsanlive2000@gmail.com' ]
+// // console.log(Object.entries(user))/[[ 'id', 1 ],[ 'name', 'raushan' ],[ 'email', 'rauhsanlive2000@gmail.com' ]]
+// console.log(user.hasOwnProperty('name'))//true
+// console.log(user.hasOwnProperty('fullname'))//false
+
+// const{name}=user
+// console.log(name);//raushan
+
+// const{name:myname}=user
+// console.log(myname);//raushan
+
+// function userName(name="sam"){
+//     return `${name} just logged in`
+// }
+// console.log(userName());//sam just logged in
+// console.log(userName("hitesh"))//hitesh just logged in
